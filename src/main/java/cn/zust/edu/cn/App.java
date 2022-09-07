8bf82dfd2d4d7d92530e0e3afddb941e90dcee06
czg <chengzhigang@zust.eud.cn>
2022-09-07 11:30:11 +0800

    init

---
+++ src/main/java/cn/zust/edu/cn/App.java
@@ -0,0 +1,12 @@
+package cn.zust.edu.cn;
+/**
+ * 
+ * @author czg
+ * @since 2022/9/7 11:07
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+
+    }
+}
