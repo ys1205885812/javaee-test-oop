8ad2c1b10c8d6585037d43184333c073463a6b67
czg <chengzhigang@zust.eud.cn>
2023-03-07 12:16:10 +0800

    添加point类

---
+++ src/main/java/cn/zust/edu/cn/Point.java
@@ -0,0 +1,9 @@
+package cn.zust.edu.cn;
+/**
+ * 
+ * @author czg
+ * @since 2023/3/7 10:50
+ */
+public class Point {
+
+}
---
+++ src/main/java/cn/zust/edu/cn/Rectangle.java
@@ -0,0 +1,8 @@
+package cn.zust.edu.cn;
+/**
+ * 
+ * @author czg
+ * @since 2023/3/7 10:50
+ */
+public class Rectangle {
+}
